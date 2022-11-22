87f8f7c88e3c220c5a84871760c326d85f0d3f86
Edwin Lee <leeed2001@gmail.com>
2022-11-22 07:51:59 -0600

    Add API Helpers, bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme_contents = readme_file.read_text()
 
 setup(
     name="energyplus_python_apps",
-    version="22.2.0-Beta4",
+    version="22.2.0-Beta5",
     packages=['energyplus_python_apps'],
     description="Meta-package collecting all official EnergyPlus Python utilities",
     long_description=readme_contents,
@@ -22,5 +22,6 @@ setup(
         'energyplus-pet==0.43',
         'energyplus-idd-idf-utilities==0.86',
         'energyplus-regressions==1.9.7',
+        'energyplus-api-helpers==0.2',
     ]
 )
